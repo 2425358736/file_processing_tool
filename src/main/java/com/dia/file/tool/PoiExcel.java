49dd01babc1c9763c1df569d1cfc82802600f6c8
2425358736 <2425358736@qq.com>
2019-02-19 13:55:23 +0800

    文件处理  --> 文件上传  --> 集成oss  --> 文件下载  --> excel导出

--- src/main/java/com/dia/file/tool/PoiExcel.java
+++ src/main/java/com/dia/file/tool/PoiExcel.java
@@ -1,11 +1,19 @@
 package com.dia.file.tool;
 
+import com.dia.file.enumModule.ContentType;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.omg.CORBA.portable.ValueOutputStream;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -25,7 +33,7 @@ import java.util.function.BiConsumer;
  * 版权：青岛上朝信息科技有限公司
  * <p>
  * 类功能描述
- *
+ * 生成Excel
  * @author 刘志强
  * @created 2018/5/10.
  */
@@ -43,6 +51,56 @@ public class PoiExcel {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        HSSFWorkbook workbook = getHSSFWorkbook(listHead, listDataSource);
+        try {
+            OutputStream baos = new FileOutputStream(path.toAbsolutePath().toString());
+            workbook.write(baos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dirName + "/" + fileName;
+    }
+
+
+    /**
+     * 导出
+     * @param listHead
+     * @param listDataSource
+     * @param fileName
+     * @return
+     */
+    public static ResponseEntity<byte[]> exportExcel(List<Map<String,Object>> listHead,List<Objects> listDataSource, String fileName){
+        fileName = fileName + ".xls";
+        HSSFWorkbook workbook = getHSSFWorkbook(listHead, listDataSource);
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            ContentType contentType = ContentType.ContentTypeStr("xls");
+            headers.setContentType(MediaType.valueOf(contentType.getType()));
+            headers.setContentDispositionFormData("attachment", new String(fileName.getBytes("utf-8"), "ISO8859-1"));
+            ByteArrayOutputStream outByteStream = new ByteArrayOutputStream();
+            workbook.write(outByteStream);
+            return new ResponseEntity(outByteStream.toByteArray(), headers, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static <E> void forEach(
+            Iterable<? extends E> elements, BiConsumer<Integer, ? super E> action) {
+        Objects.requireNonNull(elements);
+        Objects.requireNonNull(action);
+
+        int index = 0;
+        for (E element : elements) {
+            action.accept(index++, element);
+        }
+    }
+
+
+    public static HSSFWorkbook getHSSFWorkbook(List<Map<String,Object>> listHead,List<Objects> listDataSource) {
+
         // 创建Excel文档
         HSSFWorkbook workbook = new HSSFWorkbook();
 
@@ -85,31 +143,13 @@ public class PoiExcel {
                             } else if (StringUtils.equals(mapHead.get("column").toString(),"id")){;
                                 cell.setCellValue(finalIndex);
                             } else {
-                                    cell.setCellValue(v.toString());
+                                cell.setCellValue(v.toString());
                             }
                         }
                     }
                 });
             });
         }
-        try {
-            OutputStream baos = new FileOutputStream(path.toAbsolutePath().toString());
-            workbook.write(baos);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return dirName + "/" + fileName;
-    }
-
-
-    public static <E> void forEach(
-            Iterable<? extends E> elements, BiConsumer<Integer, ? super E> action) {
-        Objects.requireNonNull(elements);
-        Objects.requireNonNull(action);
-
-        int index = 0;
-        for (E element : elements) {
-            action.accept(index++, element);
-        }
+        return  workbook;
     }
 }
